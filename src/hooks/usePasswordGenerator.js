58ed6284a3fcefbcfd46be9f0c8393a1f1786580
Espadv69 <aitorjob69@gmail.com>
2025-03-02 19:27:47 +0100

    Fix typo in usePasswordGenerator hook and enhance password generation logic to include symbols

--- src/hooks/usePasswordGenerator.js
+++ src/hooks/usePasswordGenerator.js
@@ -4,11 +4,23 @@ export const usePasswordGenerator = () => {
   const [password, setPassword] = useState('')
   const [length, setLength] = useState(12)
   const [includeNumbers, setIncludeNumbers] = useState(true)
-  const [icludeSymbols, setIcludeSymbols] = useState(false)
+  const [includeSymbols, setIncludeSymbols] = useState(false)
 
   const generatePassword = () => {
     const numbers = '0123456789'
     const symbols = '!@#$%^&*()_+{}:"<>?[];,./`~'
     const letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+    let characters = letters
+    if (includeNumbers) characters += numbers
+    if (includeSymbols) characters += symbols
+
+    let newPassword = ''
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length)
+      newPassword += characters[randomIndex]
+    }
+
+    setPassword(newPassword)
   }
 }
