5ff9f876d31a6338b4d343a1ccebea8842ded047
Espadv69 <aitorjob69@gmail.com>
2025-03-02 19:24:54 +0100

    Add password generation function to usePasswordGenerator hook

--- src/hooks/usePasswordGenerator.js
+++ src/hooks/usePasswordGenerator.js
@@ -5,4 +5,10 @@ export const usePasswordGenerator = () => {
   const [length, setLength] = useState(12)
   const [includeNumbers, setIncludeNumbers] = useState(true)
   const [icludeSymbols, setIcludeSymbols] = useState(false)
+
+  const generatePassword = () => {
+    const numbers = '0123456789'
+    const symbols = '!@#$%^&*()_+{}:"<>?[];,./`~'
+    const letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  }
 }
