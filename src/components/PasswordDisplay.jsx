9758d18f4492662243b3dacad579b063444c4e98
Espadv69 <aitorjob69@gmail.com>
2025-03-02 19:09:03 +0100

    Implement password copying functionality in PasswordDisplay component

--- src/components/PasswordDisplay.jsx
+++ src/components/PasswordDisplay.jsx
@@ -1,3 +1,12 @@
-const PasswordDisplay = () => {}
+const PasswordDisplay = ({ password }) => {
+  const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(password)
+      alert('Password copied to clipboard!')
+    } catch (err) {
+      console.error('Failed to copy password: ', err)
+    }
+  }
+}
 
 export default PasswordDisplay
