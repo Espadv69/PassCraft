c66a623c8b7a35e295cf22c5f1b8c7ae8712cdc1
Espadv69 <aitorjob69@gmail.com>
2025-03-02 19:13:41 +0100

    Replace Hello world message with PasswordDisplay component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import PasswordDisplay from './components/PasswordDisplay'
+
 const App = () => {
-  return <h1 className="bg-blue-500 text-white p-2">Hello, world!</h1>
+  return <PasswordDisplay />
 }
 
 export default App
