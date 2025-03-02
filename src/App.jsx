fd5753e7e856c4c26d5379e22c35e2ca676b4520
Espadv69 <aitorjob69@gmail.com>
2025-03-02 19:30:40 +0100

    Integrate password generation state and functions into App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,16 @@ import Checkbox from './components/Checkbox'
 import { usePasswordGenerator } from './hooks/usePasswordGenerator'
 
 const App = () => {
-  return <PasswordDisplay />
+  const {
+    password,
+    length,
+    setLength,
+    includeNumbers,
+    setIncludeNumbers,
+    includeSymbols,
+    setIncludeSymbols,
+    generatePassword,
+  } = usePasswordGenerator()
 }
 
 export default App
