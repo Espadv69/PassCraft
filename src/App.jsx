9ba33f5ae5a2436e3956c2b7a977f1e4634f8f92
Espadv69 <aitorjob69@gmail.com>
2025-03-02 19:29:51 +0100

    Add Slider and Checkbox components to App and import usePasswordGenerator hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import PasswordDisplay from './components/PasswordDisplay'
+import Slider from './components/Slider'
+import Checkbox from './components/Checkbox'
+
+import { usePasswordGenerator } from './hooks/usePasswordGenerator'
 
 const App = () => {
   return <PasswordDisplay />
